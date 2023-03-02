b5a3a49e3587730588a0e37f7ead202b1336b856
TheProgrammer <mxolisinkosi2360@gmail.com>
2023-03-02 14:59:50 +0200

    added the time waiter functionality

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import datetime
+import time
 # creating an alarm app
 print("Welcome to the alarm app")
-time_for_alarm = input("Enter The time you wanna stop the alarm: HH-MM")
-
+time_for_alarm = input("Enter The time you wanna stop the alarm: HH:MM")
+time_for_alarm_as_list = time_for_alarm.split(":")
+# waiting until its that time
+not_yet_time = True
+while not_yet_time:
+    # check the time now
+    time_now = datetime.datetime.now()
+    hour = time_now.strftime("%H")
+    minute = time_now.strftime("%M")
+    if hour == time_for_alarm_as_list[0] and minute == time_for_alarm_as_list[1]:
+        # stop the waiting
+        not_yet_time = False
+        print(f" The time is {time_for_alarm}")
+    # waiting one minute before the repeat of the while loop
+    time.sleep(1)
